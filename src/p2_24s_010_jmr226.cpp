6c97f99711f7012be4f727358f393e50ae1b1610
jackr276 <jack_m_robbins@me.com>
2024-03-14 09:19:51 -0400

    add printing statements

--- src/p2_24s_010_jmr226.cpp
+++ src/p2_24s_010_jmr226.cpp
@@ -13,6 +13,11 @@
 
 using namespace std;
 
+
+/**
+ * The main function simply gets user input and calls the pda function, printing out the appropriate
+ * input based on the result
+ */
 int main(){
 	//Predeclare main PDA function
 	bool pda(string word);
@@ -64,20 +69,23 @@ bool pda(string word){
 		switch(currstate){
 			//start state, all strings begin here
 			case q1:
+				cout << "In q1. Read nothing, pop nothing, push '$'. Move to q2." << endl;
 				//Read nothing, pop nothing, push $(stack marker)
 				langStack.push('$');
 				//Move to q2
 				currstate = q2;
 				//we don't need the character here, simply pushing $ to stack
 				w.putback(ch);
-			
+
 				break;
 			
 			case q2:
 				//First character seen must be a
 				if(ch != 'a'){
+					cout << "In q2. Read '" << ch << "', pop nothing, push nothing. PDA crashes." << endl;
 					return false;
-				}	
+				}
+				cout << "In q2. Read 'a', pop nothing, push 'a'. Move to q3." << endl;
 				//read a, pop nothing, push a, move to q3
 				langStack.push('a');
 				currstate = q3;
@@ -87,13 +95,16 @@ bool pda(string word){
 			case q3:
 				//We can see as many b's as we want
 				if(ch == 'b'){
+					cout << "In q3. Read 'b', pop nothing, push 'b'. Move to q3." << endl;
 					langStack.push('b');
 					currstate = q3;
 				} else if(ch == 'a') {
+					cout << "In q3. Read 'a', pop nothing, push 'a'. Move to q4." << endl;
 					langStack.push('a');
 					currstate = q4;
 				} else {
 					//crash
+					cout << "In q3. Read '" << ch << "', pop nothing, push nothing. PDA crashes." << endl;
 					return false;
 				}
 
@@ -101,14 +112,18 @@ bool pda(string word){
 
 			case q4:
 				if(ch == '('){
+					cout << "In q4. Read '(', pop nothing, push '('. Move to q4." << endl;
 					langStack.push('(');
 					currstate = q4;
 				} else if(ch == '.'){
+					cout << "In q4. Read '.', pop nothing, push nothing. Move to q6." << endl;
 					currstate = q6;
 				} else if(numbers.find(ch) != string::npos){
+					cout << "In q4. Read '" << ch <<"', pop nothing, push nothing. Move to q6." << endl;
 					currstate = q5;
 				} else {
 					//crash
+					cout << "In q4. Read '" << ch << "', pop nothing, push nothing. PDA crashes." << endl;
 					return false;
 				}
 			
@@ -116,11 +131,14 @@ bool pda(string word){
 
 			case q5:
 				if(numbers.find(ch) != string::npos){
+					cout << "In q5. Read '" << ch <<"', pop nothing, push nothing. Move to q5." << endl;
 					currstate = q5;
 				} else if(ch == '.') {
+					cout << "In q5. Read '.', pop nothing, push nothing. Move to q7." << endl;
 					currstate = q7;
 				} else {
 					//crash
+					cout << "In q5. Read '" << ch << "', pop nothing, push nothing. PDA crashes." << endl;
 					return false;
 				}
 
@@ -128,9 +146,11 @@ bool pda(string word){
 
 			case q6:
 				if(numbers.find(ch) != string::npos){
+					cout << "In q6. Read '" << ch << "', pop nothing, push nothing. Move to q7." << endl;
 					currstate = q7;
 				} else {
 					//crash
+					cout << "In q6. Read '" << ch << "', pop nothing, push nothing. PDA crashes." << endl; 
 					return false;
 				}
 
@@ -138,18 +158,23 @@ bool pda(string word){
 			
 			case q7:
 				if(numbers.find(ch) != string::npos){
+					cout << "In q7. Read '" << ch << "', pop nothing, push nothing. Move to q7." << endl;
 					currstate = q7;
 				} else if(ch == ')' && langStack.top() == '(') {
+					cout << "In q7. Read ')', pop '(', push nothing. Move to q8." << endl; 
 					langStack.pop();
 					currstate = q8;
 				} else if(ch == 'a' && langStack.top() == 'a'){
+					cout << "In q7. Read 'a', pop 'a', push nothing. Move to q9." << endl;
 					//pop a
 					langStack.pop();
 					currstate = q9;
 				} else if(operators.find(ch) != string::npos) {
+					cout << "In q7. Read '" << ch << "', pop nothing, push nothing. Move to q4." << endl;
 					currstate = q4;
 				} else {
 					//crash
+					cout << "In q7. Read '" << ch << "', pop nothing, push nothing. PDA crashes." << endl;
 					return false;
 				}
 
@@ -157,15 +182,19 @@ bool pda(string word){
 
 			case q8:
 				if(ch == ')' && langStack.top() == '('){
+					cout << "In q8. Read ')', pop '(', push nothing. Move to q8." << endl;
 					langStack.pop();
 					currstate = q8;
 				} else if(operators.find(ch) != string::npos){
+					cout << "In q8. Read '" << ch << "', pop nothing, push nothing. Move to q4." << endl;
 					currstate = q4;
 				} else if(ch == 'a' && langStack.top() == 'a') {
+					cout << "In q8. Read 'a', pop 'a', push nothing. Move to q9." << endl;
 					langStack.pop();
 					currstate = q9;
 				}else {
 					//crash
+					cout << "In q8. Read '" << ch << "', pop nothing, push nothing. PDA crashes." << endl;
 					return false;
 				}
 
@@ -173,13 +202,16 @@ bool pda(string word){
 
 			case q9:
 				if(ch == 'b' && langStack.top() == 'b'){
+					cout << "In q9. Read 'b', pop 'b', push nothing. Move to q9." << endl;
 					langStack.pop();
 					currstate = q9;
 				} else if(ch == 'a' && langStack.top() == 'a'){
+					cout << "In q9. Read 'a', pop 'a', push nothing. Move to q10." << endl;
 					langStack.pop();
 					currstate = q10;
 				} else {
 					//crash
+					cout << "In q9. Read '" << ch << "', push nothing, pop nothing. PDA crashes." << endl;
 					return false;
 				}
 
@@ -188,6 +220,12 @@ bool pda(string word){
 			default:
 				return false;
 		}
+	}
+	
+	if(langStack.top() == '$'){
+		cout << "In q10. Read nothing, pop '$', push nothing. Move to q11 and accept." << endl; 
+	} else {
+		cout << "In q10. Read nothing, pop '" << langStack.top() << "', push nothing. PDA crashes" << endl;
 	}
 
 	return langStack.top() == '$';
